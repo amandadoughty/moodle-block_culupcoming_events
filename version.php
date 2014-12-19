aa659354fdb6991fc0fafe81d94a8c728a78b4d0
Amanda Doughty <amanda.doughty.1@city.ac.uk>
2014-12-19 15:42:39 +0000

    Changed version

--- version.php
+++ version.php
@@ -30,4 +30,4 @@ $plugin->version   = 2014121901;  // The current plugin version (Date: YYYYMMDDX
 $plugin->requires  = 2014101000;  // Requires this Moodle version.
 $plugin->component = 'block_culupcoming_events'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '2.0.5 (Build: 2014121901)';
+$plugin->release = '2.1.0 (Build: 2014121901)';
