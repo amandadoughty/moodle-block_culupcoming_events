00e49519a3be9f3231f6ef789caf165b11381a07
amandadoughty <amanda.doughty.1@city.ac.uk>
2016-02-08 11:45:41 +0000

    Replaced deprecated function

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016020800;  // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2013101800;  // Requires this Moodle version.
+$plugin->version   = 2016020801;  // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2015111602;  // Requires this Moodle version.
 $plugin->component = 'block_culupcoming_events'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.2.1 (Build: 2016020800)';
+$plugin->release = '2.2.2 (Build: 2016020801)';
